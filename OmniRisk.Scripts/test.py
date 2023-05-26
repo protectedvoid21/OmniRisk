5b7fd212c36b10f3c64a9c23c11c9a70dffd1311
Krzysztof Kostanecki <trojczynowiec@hotmail.com>
2023-05-26 23:40:50 +0200

    dodanieDrivera

--- OmniRisk.Scripts/test.py
+++ OmniRisk.Scripts/test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-PATH = 'C:\Program Files (x86)'
+PATH = '.'
 driver = webdriver.Edge(PATH)
 
 driver.get('https://rps.ms.gov.pl/pl-PL/Public#/home')
