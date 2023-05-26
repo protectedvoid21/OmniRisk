7ec129c59fb2e7378bd254da2031156147d930f9
Krzysztof Kostanecki <trojczynowiec@hotmail.com>
2023-05-26 23:38:52 +0200

    usunięcieTest2

--- OmniRisk.Scripts/test2/test2/test2.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-PATH = "C:\Program Files (x86)\geckodriver.exe"
-driver = webdriver.Firefox(PATH)
-
-driver.get("https://rps.ms.gov.pl/pl-PL/Public#/home")
-driver.quit()
