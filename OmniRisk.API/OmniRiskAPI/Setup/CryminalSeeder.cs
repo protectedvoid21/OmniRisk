297c95a7e3cc23265a7b2bb084f89f8c33e03752
Roman Buszko <51741354+rbuszko@users.noreply.github.com>
2023-05-27 06:51:29 +0200

    To test

---
+++ OmniRisk.API/OmniRiskAPI/Setup/CryminalSeeder.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using OmniRiskAPI.Authentication;
+using OmniRiskAPI.Models;
+using OmniRiskAPI.Persistence;
+
+namespace OmniRiskAPI.Setup {
+    public class EventSeeder {
+        public static async Task Seed(OmniRiskDbContext dbContext, UserManager<AppUser> userManager) {
+            var crimeType = new CrimeType { Id = 0, Name = "Pedofilia" };
+            using (StreamReader r = new StreamReader("data.json")) {
+                string json = r.ReadToEnd();
+                List<Person> persons = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+            
+            var @cryminalists = [ new Cryminalist { id = i, PersonId = i, crimeTypeId = 0 } for i in range(50) ]
+
+            dbContext.AddRange(crimeType);
+            dbContext.AddRange(persons);
+            dbContext.AddRange(@cryminalists);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
