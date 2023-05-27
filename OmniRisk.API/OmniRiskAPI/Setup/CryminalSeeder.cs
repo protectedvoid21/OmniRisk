8a965635a8e6411e47805e84911462b8f6d15087
protectedvoid21 <nieznanyzero@interia.pl>
2023-05-27 07:05:16 +0200

    Cryminal seeder fix

--- OmniRisk.API/OmniRiskAPI/Setup/CryminalSeeder.cs
+++ OmniRisk.API/OmniRiskAPI/Setup/CryminalSeeder.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
 using OmniRiskAPI.Authentication;
 using OmniRiskAPI.Models;
 using OmniRiskAPI.Persistence;
@@ -7,12 +8,15 @@ namespace OmniRiskAPI.Setup {
     public class CryminalSeeder {
         public static async Task Seed(OmniRiskDbContext dbContext, UserManager<AppUser> userManager) {
             var crimeType = new CrimeType { Id = 0, Name = "Pedofilia" };
-            using (StreamReader r = new StreamReader("data.json")) {
-                string json = r.ReadToEnd();
-                List<Person> persons = JsonConvert.DeserializeObject<List<Person>>(json);
-            }
+            using StreamReader r = new StreamReader("data.json");
+            string json = r.ReadToEnd();
             
-            var @cryminalists = [ new Cryminalist { id = i, PersonId = i, crimeTypeId = 0 } for i in range(50) ]
+            var persons = JsonConvert.DeserializeObject<List<Person>>(json);
+
+            var cryminalists = new List<Cryminalist>();
+            for (int i = 0; i < 50; i++) {
+                cryminalists.Add(new Cryminalist { Id = i, PersonId = i, CrimeTypeId = 0 });
+            }
 
             dbContext.AddRange(crimeType);
             dbContext.AddRange(persons);
